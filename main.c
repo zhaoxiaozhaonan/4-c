1d94f97f1671abaf5c86250c3e04523678d673d9
jkjnmlk <123@example.com>
2019-01-18 10:30:58 +0800

    增加业务1功能

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ int main()
     printf("进入学生成绩管理系统\n");
     struct student arr[1000];
     int index = 0;
+    int sum=0;
 
     while (1)
     {
@@ -86,7 +87,20 @@ int main()
         }
         if (code == 4)
         {
-            /* code */
+            
+            for(int i = 0; i < index; i++)
+            {
+                sum=arr[i].yuwen+arr[i].shuxue+arr[i].yingyu;
+                
+                if (sum<180) {
+                    printf("第%d名学生%s成绩不及格\n",i+1,arr[i].name);
+                }
+                
+            }
+            printf("筛选完成，点击回车继续\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code == 5)
         {
